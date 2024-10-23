fe8bbae52cdcac20f1bd6a910ee6367253446772
cperos-xr <cperos.xr@gmail.com>
2024-10-22 18:10:21 -1000

    Better positioning

--- Assets/Scripts/PlayerPositionManager.cs
+++ Assets/Scripts/PlayerPositionManager.cs
@@ -2,8 +2,8 @@ using System;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 using static Unity.XR.CoreUtils.XROrigin;
+
 
 // a serializable enum to determine the player's position
 [Serializable]
@@ -25,7 +25,6 @@ public class PlayerPositionManager : MonoBehaviour
     public CameraController thirdPersonCameraController;
 
     public PlayerPosition playerPosition;
-    private float yoffset;
 
     // a delagate to broadcast when the player has changed positions
     public delegate void PlayerPositionChanged(PlayerPosition playerPosition);
@@ -75,62 +74,26 @@ public class PlayerPositionManager : MonoBehaviour
         }
     }
 
-    void PlacePlayerAtLocation(Transform location)
-    {
-        yoffset = location.position.y;
-        Debug.Log("Offset should be:" + yoffset);
-        xrOrigin.RequestedTrackingOriginMode = TrackingOriginMode.Device;
-        xrOrigin.CameraFloorOffsetObject = location.gameObject;
-        xrOrigin.CameraYOffset = yoffset;
-
-        Recenter(location);
-
-        xrOrigin.transform.SetParent(location);
-        xrOrigin.transform.position = location.position;
-        xrOrigin.transform.rotation = location.rotation;
-    }
-
     public void PlacePlayerInVehicle()
     {
         playerPosition = PlayerPosition.Driving;
-        PlacePlayerAtLocation(playerDrivingPosition);
-        //yoffset = playerDrivingPosition.position.y;
-        //Debug.Log("Offset should be:" + yoffset);
-        //xrOrigin.RequestedTrackingOriginMode = TrackingOriginMode.Device;
-        //xrOrigin.CameraFloorOffsetObject = playerDrivingPosition.gameObject;
-        //xrOrigin.CameraYOffset = yoffset;
-        ////recenter the xr origin
-        //Recenter(playerDrivingPosition);
-
-
-        //xrOrigin.transform.SetParent(playerDrivingPosition);
-        //xrOrigin.transform.position = playerDrivingPosition.position;
-        //xrOrigin.transform.rotation = playerDrivingPosition.rotation;
+        //PlacePlayerAtLocation(playerDrivingPosition);
+        Recenter(playerDrivingPosition);
         thirdPersonCameraController.enabled = false;
         OnPlayerPositionChanged?.Invoke(playerPosition); // for material swap
     }
 
     public void Recenter(Transform recenterTransform)
     {
-        // Calculate the offset to move the XR Origin so that the camera aligns with the recenterTransform
-        Vector3 offset = xrOrigin.Camera.transform.position - xrOrigin.Origin.transform.position;
-        Vector3 targetPosition = recenterTransform.position - offset;
-
-        // Move the XR Origin to the new target position
-        xrOrigin.MoveCameraToWorldLocation(targetPosition);
+        xrOrigin.MoveCameraToWorldLocation(recenterTransform.position);
+        xrOrigin.MatchOriginUpCameraForward(recenterTransform.up, recenterTransform.forward);
     }
 
     public void PlacePlayerOnGun()
     {
         playerPosition = PlayerPosition.Gunner;
-        //xrOrigin.RequestedTrackingOriginMode = TrackingOriginMode.Device;
-        PlacePlayerAtLocation(playerGunnerPosition);
-        //xrOrigin.CameraFloorOffsetObject = playerGunnerPosition.gameObject;
-        //xrOrigin.CameraYOffset = 0.0f;
-        //playerPosition = PlayerPosition.Gunner;
-        //xrOrigin.transform.SetParent(playerGunnerPosition);
-        //xrOrigin.transform.position = playerGunnerPosition.position;
-        //xrOrigin.transform.rotation = playerGunnerPosition.rotation;
+        //PlacePlayerAtLocation(playerGunnerPosition);
+        Recenter(playerGunnerPosition);
         thirdPersonCameraController.enabled = false;
         OnPlayerPositionChanged?.Invoke(playerPosition);
     }
